1046ce7f63e8bda986d1f419962b7ca871f461a3
juan-mendes-DEV <jp436807@gmail.com>
2024-10-30 15:07:00 -0300

    fazendo ultimos ajustes

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,5 @@
 import { LogoContainer, LogoContainerLink } from './styles'
-import logo from '../../assets/icons/logo.png'
+import logo from '../../../public/imgs/logo.png'
 
 type LogoProps = {
   kind: 'image' | 'link'
